a9e151ee4579c17026e85da6b26782b9959ca189
paula <paula@676dea2c-8a08-0410-817f-c220af3ca6b2>
2015-02-12 18:34:49 +0000

    Arreglado error de cuando no se ingresa un context

--- visibilidad-unlp/sitios_UIDs/trunk/plugins/Sedici-Plugin/Shortcode.php
+++ visibilidad-unlp/sitios_UIDs/trunk/plugins/Sedici-Plugin/Shortcode.php
@@ -10,69 +10,64 @@
  * Copyright (c) 2015 SEDICI UNLP, http://sedici.unlp.edu.ar
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  */
-
-function LoadShortcode($atts,$type) {
-	$shortcode = new Shortcode();
-	$shortcode->plugin_sedici($atts,$type);
+function LoadShortcode($atts, $type) {
+	$shortcode = new Shortcode ();
+	$shortcode->plugin_sedici ( $atts, $type );
 }
-
 function AuthorShortcode($atts) {
-	LoadShortcode($atts, 'author');
+	LoadShortcode ( $atts, 'author' );
 }
-
-
 function HandleShortcode($atts) {
-	LoadShortcode($atts, 'handle');
+	LoadShortcode ( $atts, 'handle' );
 }
-
-class Shortcode{
-function plugin_sedici($atts,$type) {
-	$filter = new Filter();
-	$a = shortcode_atts ($filter->default_shortcode(), $atts );
-	$a ['type'] = $type;
-	if (is_null ( $a ['context'] )) {
-		return "Ingrese un context";
-	}
-	$description = $a ['description'] === 'true' ? "description" : false;
-	$date = $a ['date'] === 'true' ? true : false;
-	$show_author = $a ['show_author'] === 'true' ? true : false;
-	$cache = $a ['cache'];
-	$context = $a ['context'];
-	$all = $a ['all'] === 'true' ? true : false;
-	$max_results = $a ['max_results'];
-	$maxlenght = $a ['max_lenght'];
-	
-	$util = new Query();
-	$subtypes = $filter->subtypes();
-	// $subtypes: all names of subtypes
-	$selected_subtypes = array ();
-	$groups = array ();
-	// $groups: groups publications by subtype
-	foreach ( $subtypes as $o ) {
-		//compares the user marked subtypes, if TRUE, save the subtype.
-		$subtype = $filter->convertirEspIng ( $o );
-		if ('true' === $a [$subtype]) {
-			array_push ( $selected_subtypes, $o );
-			$groups [$o] = array ();
+class Shortcode {
+	function plugin_sedici($atts, $type) {
+		$filter = new Filter ();
+		$a = shortcode_atts ( $filter->default_shortcode (), $atts );
+		if (is_null ( $a ['context'] )) {
+			echo "Ingrese un context";
+		} else {
+			$a ['type'] = $type;
+			$description = $a ['description'] === 'true' ? "description" : false;
+			$date = $a ['date'] === 'true' ? true : false;
+			$show_author = $a ['show_author'] === 'true' ? true : false;
+			$cache = $a ['cache'];
+			$context = $a ['context'];
+			$all = $a ['all'] === 'true' ? true : false;
+			$max_results = $a ['max_results'];
+			$maxlenght = $a ['max_lenght'];
+			
+			$util = new Query ();
+			$subtypes = $filter->subtypes ();
+			// $subtypes: all names of subtypes
+			$selected_subtypes = array ();
+			$groups = array ();
+			// $groups: groups publications by subtype
+			foreach ( $subtypes as $o ) {
+				// compares the user marked subtypes, if TRUE, save the subtype.
+				$subtype = $filter->convertirEspIng ( $o );
+				if ('true' === $a [$subtype]) {
+					array_push ( $selected_subtypes, $o );
+					$groups [$o] = array ();
+				}
+			}
+			$thesis = $a ['thesis'] === 'true' ? true : false;
+			if ($thesis) {
+				// if thesis is true, save subtypes thesis
+				$all_thesis = $filter->vectorTesis ();
+				// $all_thesis: all subtypes thesis
+				foreach ( $all_thesis as $o ) {
+					array_push ( $selected_subtypes, $o );
+					$groups [$o] = array ();
+				}
+			}
+			$groups = $util->group_subtypes ( $type, $all, $context, $selected_subtypes, $groups, $cache );
+			if (! $all) {
+				// elements to view publications by subtypes
+				$groups = $util->view_subtypes ( $groups, $type, $context );
+			}
+			$attribute = $util->group_attributes ( $description, $date, $show_author, $max_results, $context, $maxlenght );
+			$util->render ( $type, $all, $groups, $attribute );
 		}
 	}
-	$thesis = $a ['thesis'] === 'true' ? true : false;
-	if ($thesis) {
-		//if thesis is true, save subtypes thesis
-		$all_thesis = $filter->vectorTesis ();
-		// $all_thesis: all subtypes thesis
-		foreach ( $all_thesis as $o ) {
-			array_push ( $selected_subtypes, $o );
-			$groups [$o] = array ();
-		}
-	}
-	$groups = $util->group_subtypes ( $type, $all, $context, $selected_subtypes, $groups,$cache );
-	if (! $all) {
-		//elements to view publications by subtypes
-		$groups = $util->view_subtypes ( $groups, $type, $context );
-	}
-	$attribute = $util->group_attributes( $description, $date, $show_author, $max_results, $context ,$maxlenght );
-	$util->render ( $type, $all, $groups, $attribute );
-	return;
-}
 }
