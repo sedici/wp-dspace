4c272e262ea73445ea8d8ea10df1f036d92a6937
Paula <paulasl92@gmail.com>
2015-02-12 18:26:10 +0000

    Shortcode author / handle creados

--- util/Filter.php
+++ util/Filter.php
@@ -21,7 +21,28 @@ class Filter {
 		);
 		$this->thesis= array ( "Tesis de doctorado",	"Tesis de grado","Tesis de maestria" );
 	}
-	
+	public function default_shortcode(){
+		return ( array (
+			'type' => null,
+			'context' => null,
+			'max_results' => 0,
+			'max_lenght' => 0,
+			'all' => false,
+			'description' => false,
+			'date' => false,
+			'show_author' => false,
+			'cache' => 604800,
+			'article' => false,
+			'preprint' => false,
+			'book' => false,
+			'working_paper' => false,
+			'technical_report' => false,
+			'conference_object' => false,
+			'revision' => false,
+			'work_specialization' => false,
+			'thesis' => false 
+	));
+	}
 	public function subtypes() {
 		return $this->subtypes;
 	}
