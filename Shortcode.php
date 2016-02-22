e58d4f700ae884fc7c307eccc8d6f0d81280526e
Gonzalo Villarreal <gonetil@gmail.com>
2016-02-22 10:50:34 -0300

    saco la informacion del plugin del archivo Shortcode.php

--- Shortcode.php
+++ Shortcode.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Plugin Name: Sedici-Plugin
- * Plugin URI: http://sedici.unlp.edu.ar/
- * Description: This plugin connects the repository SEDICI in wordpress, with the purpose of showing the publications of authors or institutions
- * Version: 1.0
- * Author: SEDICI - Paula Salamone Lacunza
- * Author URI: http://sedici.unlp.edu.ar/
- * Copyright (c) 2015 SEDICI UNLP, http://sedici.unlp.edu.ar
- * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
- */
 function LoadShortcode($atts, $type) {
 	$shortcode = new Shortcode ();
 	$shortcode->plugin_sedici ( $atts, $type );
