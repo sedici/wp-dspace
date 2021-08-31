5c4cb0f050fb6a36cda014bd88ac14799487ab79
Ezequiel Manzur <ezemanzur04@gmail.com>
2021-08-31 13:26:01 -0300

    Comtabilidad con jQuery en admin area

--- inc/admin/js/wp-dspace-admin.js
+++ inc/admin/js/wp-dspace-admin.js
@@ -1,43 +1,3 @@
-jQuery(document).ready(function () {
-    
-    // inicializacion
-    var divConditionalDescription = 'div.conditionally-description';
-    var divDescription = 'div.description-ds';
-    var conditionalDescription = 'p.conditionally-description';
-    var description = 'p.description-ds input:checkbox';
-
-    var conditionalFilter = 'p.conditionally-filter';
-    var checkFilter = 'p.show-filter';
-
-    var conditionalLimit = 'p.conditionally-limit';
-    var checkLimit = 'p.limit';
-
-    var filters = new Array({ selector: description, conditional: conditionalDescription },
-        { selector: divDescription, conditional: divConditionalDescription },
-        { selector: checkFilter, conditional: conditionalFilter },
-        { selector: checkLimit, conditional: conditionalLimit });
-        // binding
-    jQuery.each(jQuery(".checkSupport"),function(){
-        
-    })
-    jQuery.each(filters, function (i, value) {
-        jQuery(value.selector).on('change', function () {
-            jQuery(value.conditional).toggle();
-        });
-    });
-
-    jQuery("#origen select").on('change', function () {
-        var selection = jQuery(this).val();
-        if (jQuery(`#option_${selection}`).attr('support')){
-            jQuery("div.conditional_config").show();
-        }else {
-            jQuery("div.conditional_config").hide();
-        }
-    });
-
-});
-
-
 function justNumbers(e) {
     var keynum = window.event ? window.event.keyCode : e.which;
     if ((keynum == 8) || (keynum == 9)) return true;
@@ -68,3 +28,45 @@ function justNumbers(e) {
 
 
 // }
+
+(function( $ ) {
+	'use strict';
+
+	$(document).ready(function(){
+        var divConditionalDescription = 'div.conditionally-description';
+        var divDescription = 'div.description-ds';
+        var conditionalDescription = 'p.conditionally-description';
+        var description = 'p.description-ds input:checkbox';
+    
+        var conditionalFilter = 'p.conditionally-filter';
+        var checkFilter = 'p.show-filter';
+    
+        var conditionalLimit = 'p.conditionally-limit';
+        var checkLimit = 'p.limit';
+    
+        var filters = new Array({ selector: description, conditional: conditionalDescription },
+            { selector: divDescription, conditional: divConditionalDescription },
+            { selector: checkFilter, conditional: conditionalFilter },
+            { selector: checkLimit, conditional: conditionalLimit });
+            // binding
+        $.each($(".checkSupport"),function(){
+            
+        })
+        $.each(filters, function (i, value) {
+            $(document.body).on('change',value.selector, function () {
+                $(value.conditional).toggle();
+            });
+        });
+    
+        $(document.body).on('change', "#origen select",function () {
+            var selection = $(this).val();
+            if ($(`#option_${selection}`).attr('support')){
+                $("div.conditional_config").show();
+            }else {
+                $("div.conditional_config").hide();
+            }
+        });
+    
+		
+	});
+})( jQuery );
--- media/js/scrips.js
+++ media/js/scrips.js
@@ -1,44 +1,48 @@
-jQuery(document).ready(function() {
-	// inicializacion
+function justNumbers(e) {
+    var keynum = window.event ? window.event.keyCode : e.which;
+    if ((keynum == 8) || (keynum == 9)) return true;
+    return /\d/.test(String.fromCharCode(keynum));
+}
+
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+        // inicializacion
         var divConditionalDescription = 'div.conditionally-description';
         var divDescription = 'div.description-ds';
-	var conditionalDescription = 'p.conditionally-description';
-	var description = 'p.description-ds input:checkbox';
-	
-	var conditionalFilter = 'p.conditionally-filter';
-	var checkFilter = 'p.show-filter';
-	
-	var conditionalLimit = 'p.conditionally-limit';
-	var checkLimit = 'p.limit';
-        
-        var filters = new Array ( {selector:description, conditional:conditionalDescription },
-                                  {selector:divDescription, conditional:divConditionalDescription },
-                                  {selector:checkFilter, conditional:conditionalFilter},
-                                  {selector:checkLimit, conditional:conditionalLimit});
+        var conditionalDescription = 'p.conditionally-description';
+        var description = 'p.description-ds input:checkbox';
 
-	// binding
-    jQuery.each( filters, function( i, value ) {
-            jQuery(value.selector).live('change', function() {
-		jQuery(value.conditional).toggle();
-         }); 
+        var conditionalFilter = 'p.conditionally-filter';
+        var checkFilter = 'p.show-filter';
+
+        var conditionalLimit = 'p.conditionally-limit';
+        var checkLimit = 'p.limit';
+
+        var filters = new Array({ selector: description, conditional: conditionalDescription },
+            { selector: divDescription, conditional: divConditionalDescription },
+            { selector: checkFilter, conditional: conditionalFilter },
+            { selector: checkLimit, conditional: conditionalLimit });
+
+        // binding
+        $.each(filters, function (i, value) {
+            $(document.body).on('change',value.selector, function () {
+                $(value.conditional).toggle();
+            });
+        });
+
+        $(document.body).on('change',"#origen select", function () {
+            var selection = $(this).val();
+            switch (selection) {
+                case "conicet":
+                    $("div.conditional_config").hide()
+                    break;
+                default:
+                    $("div.conditional_config").show()
+            }
+        });
+
+
     });
-        
-    jQuery( "#origen select").live('change',function(){
-    var selection = jQuery(this).val();
-    switch(selection){
-        case "conicet":
-            jQuery("div.conditional_config").hide()
-        break;
-        default:
-            jQuery("div.conditional_config").show()
-    }
-    });
-
-});
-
-
-function justNumbers(e) {
-	var keynum = window.event ? window.event.keyCode : e.which;
-	if ((keynum == 8) || (keynum == 9)) return true;
-	return /\d/.test(String.fromCharCode(keynum));
-}
+})(jQuery);
