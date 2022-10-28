681e7ec8d95d7f73fd37ef063a1f3cb12bbe90eb
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2022-10-28 08:38:48 -0300

    Agrego inicialización de valores para los repositorios

--- inc/core/class-activator.php
+++ inc/core/class-activator.php
@@ -26,13 +26,13 @@ class Activator
         $args = array( 'repositorios' => array());
 
         array_push($args['repositorios'],array('id'=>uniqid(), 'name' => 'sedici',
-            'domain' => 'sedici.unlp.edu.ar','support'=>true,'protocol'=>'http','subtype' =>'sedici.subtype:','handle'=>'scope','author'=>'author:','subject'=>'subject:','degree'=>'thesis.degree.name','base_path'=>'/open-search/discover','format' => 'atom','query'=>'query','default_query'=>"")
+            'domain' => 'sedici.unlp.edu.ar','support'=>true,'protocol'=>'http','subtype' =>'sedici.subtype:','queryMethod'=>false,'handle'=>'scope','author'=>'author:','subject'=>'subject:','degree'=>'thesis.degree.name','base_path'=>'/open-search/discover','format' => 'atom','query'=>'query','default_query'=>"")
         );
         array_push($args['repositorios'],array('id'=>uniqid(), 'name' => 'cic',
-            'domain' => 'digital.cic.gba.gob.ar','support'=>true,'protocol'=>'https','subtype' =>'dc.type:','handle'=>'scope','author'=>'author:','subject'=>'subject:','base_path'=>'/open-search/discover','format' => 'atom','query'=>'query','default_query'=>"" )
+            'domain' => 'digital.cic.gba.gob.ar','support'=>true,'protocol'=>'https','subtype' =>'dc.type:','queryMethod'=>true, 'apiUrl'=>'https://host170.sedici.unlp.edu.ar/server/api' ,'handle'=>'scope','author'=>'author:','subject'=>'subject:','base_path'=>'/open-search/discover','format' => 'atom','query'=>'query','default_query'=>"" )
 		);
 		array_push($args['repositorios'],array('id'=>uniqid(), 'name' => 'conicet',
-            'domain' => 'ri.conicet.gov.ar','support'=>false,'protocol'=>'https','subtype' =>'','handle'=>'scope','author'=>'dc.contributor.author:','base_path'=>'/open-search/discover','format' => 'atom','query'=>'query','default_query'=>"*" )
+            'domain' => 'ri.conicet.gov.ar','support'=>false,'protocol'=>'https','subtype' =>'','queryMethod'=>false,'handle'=>'scope','author'=>'dc.contributor.author:','base_path'=>'/open-search/discover','format' => 'atom','query'=>'query','default_query'=>"*" )
         );
 
         update_option('config_repositorios',$args);
