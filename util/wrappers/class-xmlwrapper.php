7bf9fa7038ca697bba55895115a44e4c1eeeb244
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2023-07-11 10:25:41 -0300

    Fix: Ordenamiento de fechas

--- util/wrappers/class-xmlwrapper.php
+++ util/wrappers/class-xmlwrapper.php
@@ -26,6 +26,9 @@ class xmlWrapper extends genericDocumentWrapper {
         if(!empty($dc_values->date)){
             return date_create($dc_values->date);
         }
+        else{
+            return date_create($this->date);
+        }
     }
 
     public function set_authors(){
