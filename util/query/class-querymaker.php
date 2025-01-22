cfec944b8a4c0a7a6ada4ac434034ac1e306b29b
Lautaro <ljosinsaller@gmail.com>
2025-01-22 13:08:42 -0300

    Agrego comentarios a funciones en las clases simplexmlmodel y querymaker

--- util/query/class-querymaker.php
+++ util/query/class-querymaker.php
@@ -25,21 +25,52 @@ abstract class queryMaker
 
     public abstract function getPublications($all, $queryStandar, $cache, $subtypes_selected, $max_results);
     
+
+    /**
+     * Obtiene el modelo asociado a la instancia.
+     *
+     * @return SimpleXMLModel
+     */
     public function get_model()
     {
         return $this->model;
     }
 
+    /**
+     * Configura un valor de comparación en la instancia de XmlOrder.
+     *
+     * @param mixed $value El valor de comparación que se establecerá.
+     * @return void
+     */
     public function setCmp($value)
     {
         $this->order->setCmp($value);
     }
     
+    /**
+     * Divide una cadena de entrada en un array utilizando el delimitador ';'.
+     *
+     * @param string $imput La cadena de entrada que se desea dividir.
+     * @return array Un array de cadenas resultante de dividir la entrada.
+     */
     public function splitImputs($imput)
     {
         return explode(';', $imput);
     }
 
+    /**
+     * Agrupa varios atributos en un array asociativo.
+     *
+     * @param string $description Una descripción del recurso.
+     * @param string $date La fecha asociada al recurso.
+     * @param bool $show_author Indica si se debe mostrar el autor.
+     * @param int $maxlenght La longitud máxima permitida para los textos.
+     * @param bool $show_subtypes Indica si se deben mostrar los subtipos.
+     * @param bool $share Indica si el recurso puede ser compartido.
+     * @param bool $show_videos (Opcional) Indica si se deben incluir videos. Por defecto es `false`.
+     * @param int|null $max_results (Opcional) El número máximo de resultados permitidos. Por defecto es `null`.
+     * @return array Un array asociativo con los atributos agrupados.
+     */
     function group_attributes($description, $date, $show_author, $maxlenght, $show_subtypes, $share, $show_videos = false, $max_results = null)
     {
         return (array(
